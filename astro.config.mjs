88b4b7075d5ae0b45ba8b04729b6c8d04118daba
Raul Ospina <raminka13@hotmail.com>
2024-11-11 16:13:42 -0500

    Update: redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,9 +8,18 @@ import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://elite-flight.netlify.app/",
+  site: "https://eliteproaviation.com/",
   integrations: [mdx(), sitemap(), tailwind(), react(), partytown()],
   redirects: {
     "/posts/[...slug]": "/blog/[...slug]",
+    "/multi-engine-rating": "/flight-training/multi-engine-rating",
+    "/private-pilot-training-program":
+      "/flight-training/private-pilot-training",
+    "/instrument-rating-course": "/flight-training/instrument-rating",
+    "/commercial-pilot-license": "/flight-training/commercial-pilot-training",
+    "/aircraft-management": "/aircraft-services",
+    "/aircraft-brokerage": "/aircraft-services/aircraft-brokerage",
+    "/aircraft-for-sale": "/aircraft-services/aircraft-for-sale",
+    "/aircraft-rental-other-services": "/aircraft-services/aircraft-rentals",
   },
 });
